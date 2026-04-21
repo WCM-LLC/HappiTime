72618b90df484f759da3adf0ad3ffd9aa0915a6f
Juan Williams <jugganaught@gmail.com>
2026-04-20 19:28:01 -0500

    Google Tag Manager - React Native

--- apps/directory/src/app/layout.tsx
+++ apps/directory/src/app/layout.tsx
@@ -88,6 +88,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             }}
           />
         )}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8MZMX2GH4E"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-8MZMX2GH4E');`,
+          }}
+        />
       </head>
       <body className="min-h-screen">
         {/* GTM noscript fallback */}
